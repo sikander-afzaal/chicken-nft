b049eb7017513fa291f99ce40c908a93488c88da
sikander-afzaal <sikander.aaa@gmail.com>
2022-12-24 00:17:07 +0500

    asdasd

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,32 +1,44 @@
 import "./Landing.css";
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Landing = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
   return (
     <div className="container landing-wrapp">
+      {menuToggle ? (
+        <AiOutlineClose
+          className="menu-toggle"
+          onClick={() => setMenuToggle(false)}
+        />
+      ) : (
+        <FiMenu className="menu-toggle" onClick={() => setMenuToggle(true)} />
+      )}
       {/* <img src="/floating.png" className="bg" alt="" /> */}
       <img src="/egg1.png" alt="" className="floating egg1" />
       <img src="/egg2.png" alt="" className="floating egg2" />
       <img src="/egg3.png" alt="" className="floating egg3" />
       <img src="/egg4.png" alt="" className="floating egg4" />
-      <img src="/egg5.png" alt="" className="floating egg5" />
-      <div className="landing-div">
-        <div className="left-btns">
-          <div className="btn-div">
-            <img src="/deck.png" alt="" className="btn-bg deck" />
-          </div>
-          <div className="btn-div">
-            <img src="/cluckers.png" alt="" className="btn-text" />
-            <img src="/egg-btn.png" alt="" className="btn-bg" />
-          </div>
-          <div className="btn-div">
-            <img src="/traits.png" alt="" className="btn-text" />
-            <img src="/egg-btn.png" alt="" className="btn-bg" />
-          </div>
-          <div className="btn-div">
-            <img src="/my.png" alt="" className="btn-text" />
-            <img src="/egg-btn.png" alt="" className="btn-bg" />
-          </div>
+      <img src="/egg5.png" alt="" className="floating egg5" />{" "}
+      <div className={`left-btns ${menuToggle ? "active-side" : ""}`}>
+        <div className="btn-div">
+          <img src="/deck.png" alt="" className="btn-bg deck" />
         </div>
+        <div className="btn-div">
+          <img src="/cluckers.png" alt="" className="btn-text" />
+          <img src="/egg-btn.png" alt="" className="btn-bg" />
+        </div>
+        <div className="btn-div">
+          <img src="/traits.png" alt="" className="btn-text" />
+          <img src="/egg-btn.png" alt="" className="btn-bg" />
+        </div>
+        <div className="btn-div">
+          <img src="/my.png" alt="" className="btn-text" />
+          <img src="/egg-btn.png" alt="" className="btn-bg" />
+        </div>
+      </div>
+      <div className="landing-div">
         <div className="connect-div">
           <img src="/connect.png" alt="" className="connect-bg" />
           <img src="/connect-text.png" alt="" className="connect-text" />
@@ -39,23 +51,7 @@ const Landing = () => {
             <img src="/mint-text.png" alt="" className="mint-text" />
             <img src="/mint.png" alt="" className="mint-bg" />
           </div>
-          <div className="left-btns">
-            <div className="btn-div">
-              <img src="/deck.png" alt="" className="btn-bg deck" />
-            </div>
-            <div className="btn-div">
-              <img src="/cluckers.png" alt="" className="btn-text" />
-              <img src="/egg-btn.png" alt="" className="btn-bg" />
-            </div>
-            <div className="btn-div">
-              <img src="/traits.png" alt="" className="btn-text" />
-              <img src="/egg-btn.png" alt="" className="btn-bg" />
-            </div>
-            <div className="btn-div">
-              <img src="/my.png" alt="" className="btn-text" />
-              <img src="/egg-btn.png" alt="" className="btn-bg" />
-            </div>
-          </div>
+
           <div className="chicken-div">
             <img src="/chick1.png" alt="" />
             <img src="/chick2.png" alt="" />
@@ -65,6 +61,9 @@ const Landing = () => {
           </div>
         </div>
       </div>
+      {menuToggle && (
+        <div className="overlay" onClick={() => setMenuToggle(false)}></div>
+      )}
     </div>
   );
 };
