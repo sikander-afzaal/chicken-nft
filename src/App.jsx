cf9d00f2e17f9bc2bb7c80ff63919374a62c6526
sikander-afzaal <sikander.aaa@gmail.com>
2022-12-26 00:09:48 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Loader from "./components/Loader/Loader";
 import Cluckerz from "./pages/Cluckerz/Cluckerz";
 import Landing from "./pages/landing/Landing";
 import Traits from "./pages/Traits/Traits";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
   return (
     <div className="App">
+      {isLoading && <Loader />}
       <Routes>
         <Route element={<Landing />} path="/" />
         <Route element={<Traits />} path="/traits" />
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <img src="/loader.gif" alt="" />
+    </div>
+  );
+};
+
+export default Loader;
